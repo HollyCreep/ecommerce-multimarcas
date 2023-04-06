94ccb941225f9ed8c604794a090e3c0a76582d5a
LUCAS FERREIRA RODRIGUES DE MORAIS <lucasferreirarm@gmail.com>
2023-04-06 09:08:48 -0300

    Alterando endpoint de produtos

--- composables/apis/products/products.api.ts
+++ composables/apis/products/products.api.ts
@@ -4,8 +4,8 @@ import type { IProduct } from '~~/types/product'
 export const useProductsApi = () => {
   const api = useApi()
 
-  const fetchProducts = (options?: RequestOptions<IProduct[]>) => api.get<IProduct[]>('/products', options)
-  const getProduct = (sku: number | string, options?: RequestOptions<IProduct>) => api.get<IProduct>(`/products/${sku}`, options)
+  const fetchProducts = (options?: RequestOptions<IProduct[]>) => api.get<IProduct[]>('/planos', options)
+  const getProduct = (sku: number | string, options?: RequestOptions<IProduct>) => api.get<IProduct>(`/planos/${sku}`, options)
 
   return { getProduct, fetchProducts }
 }
